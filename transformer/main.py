1b6577577be2d5a299e9f05cd39c3e2902f84858
Caleb McIrvin <calebmcirvin111@gmail.com>
2024-09-21 20:12:22 -0400

    Cleaning up commented code

--- transformer/main.py
+++ transformer/main.py
@@ -423,9 +423,6 @@ class SimpleLossCompute:
 
 def example_simple_model():
     V = 11
-    criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
-
-    # h is number of heads
 
     model = EncoderDecoder(
         h=8,
@@ -446,35 +443,6 @@ def example_simple_model():
         logger=tb_logger
     )
     trainer.fit(model, data_gen(V, batch_size, 400))
-
-#    optimizer = torch.optim.Adam(
-#        model.parameters(), lr=0.5, betas=(0.9, 0.98), eps=1e-9
-#    )
-#    scheduler = LambdaLR(
-#        optimizer=optimizer,
-#        lr_lambda=lambda step: 1.0
-#        * (
-#            model.src_embed[0].d_model ** (-0.5)
-#            * min(max(step, 1) ** (-0.5), max(step, 1) * 400 ** (-1.5))
-#        ),
-#    )
-#    for _ in range(20):
-#        model.train()
-#        run_epoch(
-#            data_gen(V, batch_size, 20),
-#            model,
-#            SimpleLossCompute(model.generator, criterion),
-#            optimizer=optimizer,
-#            scheduler=scheduler,
-#            mode="train",
-#        )
-#        model.eval()
-#        run_epoch(
-#            data_gen(V, batch_size, 5),
-#            model,
-#            SimpleLossCompute(model.generator, criterion),
-#            mode="eval",
-#        )
 
     model.eval()
     src = torch.LongTensor([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]])
